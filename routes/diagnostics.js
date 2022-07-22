3752eb1a8a937b91ac3579cb7dbcd19a85053b3b
hmanjun <harshith.hm03@gmail.com>
2022-07-22 12:12:25 -0700

    Add post listener for diagnostics

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,30 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostic.json').then((data) => {
+    res.json(JSON.parse(data))
+  })
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const {user, tip,topic} = req.body.errors
+  if(user && tip && topic){
+    const diagFile = {
+      "time": "new",
+      "error_id": "new",
+      "errors": {
+        tip,
+        topic,
+        user
+      }
+    }
+    readAndAppend(diagFile,'./db/diagnostic.json')
+    res.json(`${req.method} method recieved, and saved`)
+  } else {
+    res.json("A failure occured")
+  }
 });
 
 module.exports = diagnostics;
